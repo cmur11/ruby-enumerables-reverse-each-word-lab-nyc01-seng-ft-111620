c75cfc2376a6bbfdf08ef445e8d8233f35dc8123
Conor Murnane <conormurnane11@gmail.com>
2020-11-04 00:07:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ require 'pry'
 def reverse_each_word(sentence)
   #binding.pry
   split_sentence = sentence.split(" ")
-  reverse_sentence = " "
+
  split_sentence.each do |word|
    #binding.pry
   reverse_sentence = word.reverse
